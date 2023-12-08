480b818cc39f500604c2c55e78699673917e3c9e
Umar Jamil <hkproj@gmail.com>
2023-12-08 14:47:17 +0800

    added global_step to tqdm progbar during train

--- train.py
+++ train.py
@@ -266,7 +266,7 @@ def train_model(config):
 
             # Compute the loss using a simple cross entropy
             loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
-            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}", "global_step": global_step})
         
             if config['global_rank'] == 0:
                 # Log the loss
