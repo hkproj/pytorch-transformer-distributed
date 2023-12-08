f296354ac170fa9aff879871174785b3d1b1a6db
Umar Jamil <hkproj@gmail.com>
2023-12-08 14:24:02 +0800

    fixed base config

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 8,
-        "num_epochs": 20,
+        "batch_size": 4,
+        "num_epochs": 30,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
